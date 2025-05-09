ce20f29db808bf84ec68f5cf77db852077d232cd
Sai-Raj <sairaj.vadeghar@gmail.com>
2025-05-10 00:57:32 +0530

    slightly updated the landing page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Navigate // Import Navigate if you want to redirect from "/"
 } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
+// import './App.css'; // You can keep or remove this based on your needs
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} /> {/* Default route */}
+        {/* Option 1: Make LandingPage the default for "/" */}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/landing" element={<LandingPage />} />
+        
+        {/* Option 2: Redirect "/" to "/landing" (if you prefer /landing as the explicit path) */}
+        {/* <Route path="/" element={<Navigate replace to="/landing" />} /> */}
+        {/* <Route path="/landing" element={<LandingPage />} /> */}
+
         <Route path="/dashboard" element={<Dashboard />} />
+        {/* You can add a 404 Not Found route here later */}
       </Routes>
     </Router>
   );
